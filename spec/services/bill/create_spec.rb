adb8831145f64b734ba3c78d524975b0ca012823
Carlos Zansavio <undefined751@gmail.com>
2018-08-11 16:43:55 -0300

    fix test error

--- spec/services/bill/create_spec.rb
+++ spec/services/bill/create_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Bill::Create do
   let(:bill_type) { create(:bill_type) }
   let(:bill_month) { create(:bill_month) }
   let(:params) { {paid_value: 5, month_value: 6, paid_date: Date.today, 
-               maturity_date: Date.today, bill_month: bill_month, bill_type: bill_type} }
+               maturity_date: Date.today, bill_month_id: bill_month.id, bill_type_id: bill_type.id} }
   
 	
   it "creates a bill using bill service" do
