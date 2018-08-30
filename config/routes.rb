7fd7c67dcd5dd6527be75c6e6786efe6a9d24198
Carlos Zansavio <undefined751@gmail.com>
2018-08-30 13:28:51 -0300

    added authetication for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
+  }, path: '/', path_names: {
+      sign_in: 'login', sign_out: 'logout',
+      password: 'secret', confirmation: 'verification',
+      registration: 'register', edit: 'edit/profile'
   }
-  resources :bills do
-    resources :bill_apportionments
-  end  
-  #resources :bill_months
-  resources :bill_types do
-    resources :bill_splitings
+  authenticate :user do
+    resources :bills do
+      resources :bill_apportionments
+    end  
+    resources :bill_types do
+      resources :bill_splitings
+    end
   end
-  root to: "bill_types#index"
+
+  authenticated :user do
+    root 'bill_types#index', as: :authenticated_root
+  end
+  root to: "home#index"
   #get 'bill_types/:id/split', action: :split, controller: 'bill_types', as: :bill_types_split
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
